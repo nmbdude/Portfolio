0c8d2c5dd003c125c910f2fba0558211072e7291
nmbdude <nmb7888@rit.edu>
2025-10-28 18:26:08 -0400

    Index done

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const navList = document.querySelector('.nav-list');
+const navListClose = document.querySelector('.nav-list-close');
 
 hamburger.addEventListener('click', function() {
-    navMenu.classList.toggle('show');
+    navList.setAttribute('style', 'display: absolute; position: fixed; z-index: 1000; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.9);');
+    navListClose.setAttribute('style', 'display: block; position: absolute; top: 20px; right: 20px; font-size: 30px; color: white; cursor: pointer;');
+});
+
+navListClose.addEventListener('click', function() {
+    navList.setAttribute('style', 'display: none;');
 });
