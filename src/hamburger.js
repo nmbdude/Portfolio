2a3321a7438bbc64a5bdbef777669aa7ce0ff880
nmbdude <nmb7888@rit.edu>
2025-10-29 16:04:57 -0400

    Contact page almost done. Just need to make it reactive.

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,9 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
-const navList = document.querySelector('.nav-list');
+const navList = document.querySelector('.nav-list-overlay');
 const navListClose = document.querySelector('.nav-list-close');
 
 hamburger.addEventListener('click', function() {
-    navList.setAttribute('style', 'display: absolute; position: fixed; z-index: 1000; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.9);');
+    navList.setAttribute('style', 'display: block; position: fixed; z-index: 1000; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.9);');
     navListClose.setAttribute('style', 'display: block; position: absolute; top: 20px; right: 20px; font-size: 30px; color: white; cursor: pointer;');
 });
 
