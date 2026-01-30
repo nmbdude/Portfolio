9f9cce7b5c92fd9dec3e5a1822efff028907eb03
nmbdude <nmb7888@rit.edu>
2026-01-30 15:50:38 -0500

    Started updating to new look based on feedback

--- src/project.js
+++ src/project.js
@@ -1,31 +1,17 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const lightbox = document.getElementById('lightbox');
-//     const lightboxImg = document.getElementById('lightbox-img');
-//     const closeBtn = document.getElementsByClassName('close')[0];
-//     const galleryImages = document.querySelectorAll('.carousel a img');
-//
-//     galleryImages.forEach(img => {
-//         img.addEventListener('click', function() {
-//             lightbox.style.display = 'block';
-//             lightboxImg.src = this.src;
-//         });
-//     });
-//
-//     closeBtn.addEventListener('click', function() {
-//         lightbox.style.display = 'none';
-//     });
-//
-//     lightbox.addEventListener('click', function(event) {
-//         if (event.target === lightbox) {
-//             lightbox.style.display = 'none';
-//         }
-//     });
-// });
+let toggleDropdown = (dropdownElement, dropdownArrow) => {
+    if (dropdownElement.style.maxHeight && dropdownElement.style.maxHeight !== '0px') {
+        collapseDropdown(dropdownElement, dropdownArrow);
+    } else {
+        expandDropdown(dropdownElement, dropdownArrow);
+    }
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    let elemsCarousel = document.querySelectorAll('.carousel');
-    let instancesCarousel = M.Carousel.init(elemsCarousel, {dist: -200, duration: 100});
+let expandDropdown = (dropdownElement, dropdownArrow) => {
+    dropdownElement.style.maxHeight = dropdownElement.scrollHeight + 'px';
+    dropdownArrow.style.transform = 'rotate(180deg)';
+}
 
-    let elemsMaterialbox = document.querySelectorAll('.materialboxed');
-    let instancesMaterialbox = M.Materialbox.init(elemsMaterialbox);
-});
+let collapseDropdown = (dropdownElement, dropdownArrow) => {
+    dropdownElement.style.maxHeight = '0px';
+    dropdownArrow.style.transform = 'rotate(0deg)';
+}
