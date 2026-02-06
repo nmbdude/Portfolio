0c8a3ee4d4230b9df58ecd9b6311eeb65f006807
nmbdude <nmb7888@rit.edu>
2026-02-06 17:49:31 -0500

    worked on SD page

--- src/project.js
+++ src/project.js
@@ -9,9 +9,11 @@ let toggleDropdown = (dropdownElement, dropdownArrow) => {
 let expandDropdown = (dropdownElement, dropdownArrow) => {
     dropdownElement.style.maxHeight = dropdownElement.scrollHeight + 'px';
     dropdownArrow.style.transform = 'rotate(180deg)';
+    dropdownElement.style.maxHeight = dropdownElement.scrollHeight + 'px';
 }
 
 let collapseDropdown = (dropdownElement, dropdownArrow) => {
     dropdownElement.style.maxHeight = '0px';
     dropdownArrow.style.transform = 'rotate(0deg)';
+    dropdownElement.style.maxHeight = '0px';
 }
